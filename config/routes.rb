f3ee3c6708048aab434129d39ea93fda5a91dd36
Caroline Artz <ceartz@gmail.com>
2014-06-12 14:42:24 -0500

    chore: add temp file for initial deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    root to: 'high_voltage/pages#show', id: 'homepage'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
