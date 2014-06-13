7d5a485a82b52dae109c4a0557e9fb0cf6657397
Caroline Artz <ceartz@gmail.com>
2014-06-12 23:20:18 -0500

    feature: add required line to spec_helper for coveralls config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'coveralls'
+Coveralls.wear!('rails')
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
