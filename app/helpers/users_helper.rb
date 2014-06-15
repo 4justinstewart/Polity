35f6c4e05e19e64e49db7603ed8007be8384227e
Justin Stewart <stewart.justin.m@gmail.com>
2014-06-15 10:39:06 -0500

    feature: UsersHelper#alderman created

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,7 @@
 module UsersHelper
   def alderman
-    
+    address = UserAddress.find(self.user_address_id)
+    legislator = Legislator.find_by_represented_ward_id(address.ward_id)
+    User.find(legislator.id)
   end
 end
