c7d23210b8c822cd8bc223978600cc5c4cf37745
Justin Stewart <stewart.justin.m@gmail.com>
2014-06-15 11:26:16 -0500

    Constituent helper method added

--- app/helpers/legislators_helper.rb
+++ app/helpers/legislators_helper.rb
@@ -1,2 +1,7 @@
 module LegislatorsHelper
+  def constituents
+    addresses = UserAddress.where(ward_id: self.represented_ward_id)
+    address_ids = addresses.pluck(:id)
+    User.where(user_address_id: address_ids)
+  end
 end
