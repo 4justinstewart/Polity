7cfd1749a699ecbdc07cf079e325d682019fc307
Cynaria <Elizabeth.J.Cruz@gmail.com>
2014-06-15 20:25:18 -0500

    feature: Added Up and Down votes

--- app/models/ward.rb
+++ app/models/ward.rb
@@ -1,5 +1,5 @@
 class Ward < ActiveRecord::Base
-  has_many :legislators, foreign_key: :alderman_id
+  has_one :legislator, foreign_key: :alderman_id
   has_many :user_addresses
   has_many :users, through: :user_addresses
 
