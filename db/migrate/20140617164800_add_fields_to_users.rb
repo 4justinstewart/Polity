c3a32b3742952d0621188239113b912f111a3bdc
Justin Stewart <email": null>
2014-06-17 12:14:55 -0500

    bugfix: converted all twitter handles to strings to avoid instance variable TypeError

--- db/migrate/20140617164800_add_fields_to_users.rb
+++ db/migrate/20140617164800_add_fields_to_users.rb
@@ -1,6 +1,6 @@
 class AddFieldsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :img_url, :string
+    add_column :users, :img_url, :text
     add_column :users, :twitter_handle, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617012702) do
+ActiveRecord::Schema.define(version: 20140617164800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,8 @@ ActiveRecord::Schema.define(version: 20140617012702) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.text     "img_url"
+    t.string   "twitter_handle"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
