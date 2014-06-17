01efe0ce25f4611f5021a368aa1c82cf4bf31529
Oli Treadwell <olitreadwell@gmail.com>
2014-06-17 10:12:17 -0500

    feature: begin doing nested forms

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  # The path used after sign up. You need to overwrite this method
+  # in your own RegistrationsController.
+  def after_sign_up_path_for(resource)
+    '/users/update_profile/'
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    '/users/update_profile/'
+  end
+
+end
