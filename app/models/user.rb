c82151fc10e8d38b2a220335c5f80cd7b68254ae
Justin Stewart <email": null>
2014-06-16 21:05:31 -0500

    bugfix: moved User helper methods into model. Fixed missing association

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 
   belongs_to :user_address
   has_many :legislation_voices
+  has_many :legislators
 
   validates :email, presence: true, uniqueness: true
 
@@ -22,4 +23,30 @@ class User < ActiveRecord::Base
     self.user_address.ward
   end
 
+  def ward_number
+    address = UserAddress.find(self.user_address_id)
+    Ward.find_by_id(address.ward_id).ward_number
+  end
+
+  def alderman
+    address = UserAddress.find(self.user_address_id)
+    current_legislator = Legislator.where(:represented_ward_id => address.ward_id).limit(1) #TODO: REMOVE THIS LIMIT CONSTRAINT THIS LATER WhEN DATE LOGIC IS USEFUL FOR SCOPING CURRENT LEGISLATOR
+    User.where(current_legislator.alderman_id) #This is the problem
+  end
+
+
+  def street_address1
+    address = UserAddress.find(self.user_address_id)
+    address.address1
+  end
+
+  def street_address2
+    address = UserAddress.find(self.user_address_id)
+    address.address2
+  end
+
+  def zip
+    address = UserAddress.find(self.user_address_id)
+    address.zip
+  end
 end
