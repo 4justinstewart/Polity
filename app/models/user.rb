0cd9db96d5b08422dadb5820635a0d477cbf8731
Oli Treadwell <olitreadwell@gmail.com>
2014-06-18 23:39:25 -0500

    feature: adding in paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,13 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, styles:
+                              {:thumb => "100x100>" ,
+                             :medium => "300x300>"},
+                             :default_url => "/images/:style/missing.png"
+
+
+ # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   # Include default devise modules. Others available are:
