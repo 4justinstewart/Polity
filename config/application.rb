78456f45ec83367e27a0ab000be9b1468f0ad127
Oli Treadwell <olitreadwell@gmail.com>
2014-06-18 20:57:02 -0500

    bug_fix: working to deploy heroku images

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Polity
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
